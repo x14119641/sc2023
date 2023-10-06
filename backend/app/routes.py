615f33cb5f718ef3eabff9636c0c5e38f52a4101
Daniel Gil <daniel.gil@zitrogames.com>
2023-10-06 08:58:59 +0200

    Added Ping Component

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,6 +2,12 @@ from flask import current_app as app
 from flask import jsonify, request
 
 
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
+
+
 @app.route('/')
 @app.route('/index')
 def index():
