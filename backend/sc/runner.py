b5aff44f1663019ed9d35a4cdf1088e0d58308c2
Daniel Gil <daniel.gil@zitrogames.com>
2023-10-20 09:08:09 +0200

    Making nice the scarper

---
+++ backend/sc/runner.py
@@ -0,0 +1,156 @@
+from db import ConnectorDb as cdb
+from scarper import Scarper as sc
+from datetime import datetime
+import pandas as pd
+import os
+import time
+
+errors_dicts_list = []
+
+
+def main():
+    cls = sc(cdb())
+
+
+def set_up():
+    conn = cdb()
+    conn.create_schema()
+
+
+def insert_ticks():
+    conn = cdb()
+    cls = sc()
+    bulk_data = []
+    i = 0
+    for index, row in cls.tickers.iterrows():
+        i += 1
+        bulk_data.append((row['Symbol'], row["Name"]))
+        if i % 999 == 0:
+            conn.execute_query(
+                "INSERT OR IGNORE   INTO tickers(tick, name) VALUES (?,?)", bulk_data)
+            bulk_data = []
+    conn.execute_query(
+        "INSERT OR IGNORE  INTO tickers(tick, name) VALUES (?,?)", bulk_data)
+    return 'Data inserted in Tickers'
+
+
+def insert_metadata():
+    conn = cdb()
+    cls = sc()
+    bulk_data = []
+    i = 0
+    cls.tickers.fillna('', inplace=True)
+    for index, row in cls.tickers.iterrows():
+        i += 1
+        bulk_data.append((
+            row['Symbol'], row["Name"],
+            row["Last Sale"].replace('$', ''),
+            row["Net Change"],
+            row["% Change"].replace('%', ''),
+            row["Market Cap"], row["Country"],
+            row["IPO Year"], row["Volume"],
+            row["Sector"], row["Industry"]
+        ))
+        if i % 999 == 0:
+            # print(bulk_data)
+            conn.execute_query(
+                """INSERT INTO metadata(
+                    tick, name, last_sale, net_change, change_perc,
+                    market_cap, country, ipo_year, volume, sector, industry) 
+                    VALUES (?,?,?,?,?,?,?,?,?,?,?)""",
+                bulk_data)
+            bulk_data = []
+    conn.execute_query(
+        """INSERT INTO metadata(
+            tick, name, last_sale, net_change, change_perc,
+            market_cap, country, ipo_year, volume, sector, industry) 
+            VALUES (?,?,?,?,?,?,?,?,?,?,?)""",
+        bulk_data)
+    return 'Data inserted in Metadata'
+
+
+def insert_investors_data(refresh_date=None):
+    conn = cdb()
+    cls = sc()
+    bulk_data = []
+    i = 0
+    x = 0
+    file_path = 'errors_runner.csv'
+    msg = 'Starting Insert Data'
+    cls.tickers.fillna('', inplace=True)
+    for tick in cls.tickers['Symbol'][0:-1]:
+        i += 1
+        try:
+            raw_data = cls.get_data(tick)
+            data = cls.parse_data(raw_data)
+
+            bulk_data.append((
+                tick, data['SharesOutstandingPCT'],
+                data['ShareoutstandingTotal'], data['TotalHoldingsValue'],
+                data['IncreasedPositionsHolders'], data['IncreasedPositionsShares'],
+                data['DecreasedPositionsHolders'], data['DecreasedPositionsShares'],
+                data['HeldPositionsHolders'], data['HeldPositionsShares'],
+                data['TotalInstitutionalHolders'], data['TotalInstitutionalShares'],
+                data['NewPositionsHolders'], data['NewPositionsShares'],
+                data['SoldOutPositionsHolders'], data['SoldOutPositionsShares'],
+                refresh_date or ''
+            ))
+            if i % 999 == 0:
+                conn.execute_query(
+                    """INSERT INTO institutional_holdings (
+                            tick, institutional_ownership_perc, 
+                            total_shares_outstanding_millions, total_value_holdings_millions, 
+                            increased_positions_holders,increased_positions_shares,
+                            decreased_positions_holders, decreased_positions_shares,
+                            held_positions_holders,held_positions_shares,
+                            total_institutional_holders,total_institutional_shares,
+                            new_positions_holders,new_positions_shares,
+                            sold_out_positions_holders,sold_out_positions_shares,
+                            refreshed_page_date) 
+                            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)""",
+                    bulk_data)
+                
+                x+=1
+                bulk_data = []
+
+            if i == len(cls.tickers['Symbol'][0:-1])-1:
+                x+=1
+                conn.execute_query(
+                    """INSERT INTO institutional_holdings(
+                        tick, institutional_ownership_perc, 
+                        total_shares_outstanding_millions, total_value_holdings_millions, 
+                        increased_positions_holders,increased_positions_shares,
+                        decreased_positions_holders, decreased_positions_shares,
+                        held_positions_holders,held_positions_shares,
+                        total_institutional_holders,total_institutional_shares,
+                        new_positions_holders,new_positions_shares,
+                        sold_out_positions_holders,sold_out_positions_shares,
+                        refreshed_page_date) 
+                        VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)""",
+                    bulk_data)
+            msg = 'Last Batch inserted in Instutitions, Total Batches = {x}'
+
+        except Exception as e:
+            errors_dicts_list.append({
+                'tick': tick, 'position': i, 'error': str(e)
+            })
+
+    if errors_dicts_list:
+        pd.DataFrame(
+            errors_dicts_list).to_csv(
+                file_path,
+                mode='a', header=not os.path.exists(file_path))
+        msg = errors_dicts_list
+    return msg
+
+
+if __name__ == "__main__":
+    refresh_date = datetime.strptime(
+        'Wed, 09 Nov 2022 17:49:42',
+        '%a, %d %b %Y %H:%M:%S').strftime('%d-%m-%Y %H:%M:%S')
+
+    # set_up()
+    # insert_ticks()
+    # insert_metadata()
+    # insert_investors_data(datetime.now())
+    # datetime.strptime('Wed, 09 Nov 2022 17:49:42', '%a, %d %b %Y %H:%M:%S')
---
+++ backend/sc/scarper.py
@@ -0,0 +1,73 @@
+import requests, os
+import pandas as pd
+from db import ConnectorDb as cdb
+
+
+
+
+class Scarper():
+    errors_dicts_list = []
+
+    def __init__(self, tickers=True) -> None:
+        if tickers:
+            self.tickers = pd.read_csv('tickers2.csv')
+
+
+    def get_data(self, tick):
+        URL = f"https://api.nasdaq.com/api/company/{tick}/institutional-holdings?limit=15&type=TOTAL&sortColumn=marketValue&sortOrder=DESC"
+        HEADERS = {
+            "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
+            "Content-Type":"application/json"}
+
+        data = requests.get(URL,headers=HEADERS).json()
+        return data
+
+    def parse_data(self, data):
+        return {
+            "SharesOutstandingPCT":data['data']['ownershipSummary']['SharesOutstandingPCT']['value'].replace('%', ''),
+            "ShareoutstandingTotal":data['data']['ownershipSummary']['ShareoutstandingTotal']['value'],
+            "TotalHoldingsValue":data['data']['ownershipSummary']['TotalHoldingsValue']['value'].replace('$', ''),
+            "IncreasedPositionsHolders":data['data']['activePositions']['rows'][0]['holders'],
+            "IncreasedPositionsShares":data['data']['activePositions']['rows'][0]['shares'].replace(',', ''),
+            "DecreasedPositionsHolders":data['data']['activePositions']['rows'][1]['holders'],
+            "DecreasedPositionsShares":data['data']['activePositions']['rows'][1]['shares'].replace(',', ''),
+            "HeldPositionsHolders":data['data']['activePositions']['rows'][2]['holders'],
+            "HeldPositionsShares":data['data']['activePositions']['rows'][2]['shares'].replace(',', ''),
+            "TotalInstitutionalHolders":data['data']['activePositions']['rows'][3]['holders'],
+            "TotalInstitutionalShares":data['data']['activePositions']['rows'][3]['shares'].replace(',', ''),
+            "NewPositionsHolders":data['data']['newSoldOutPositions']['rows'][0]['holders'],
+            "NewPositionsShares":data['data']['newSoldOutPositions']['rows'][0]['shares'].replace(',', ''),
+            "SoldOutPositionsHolders":data['data']['newSoldOutPositions']['rows'][1]['holders'],
+            "SoldOutPositionsShares":data['data']['newSoldOutPositions']['rows'][1]['shares'].replace(',', ''),
+        }
+
+
+    def main(self):
+        file_path='outputs/errors_scarper.csv'
+        print(f'Starting Scarper; number of tickers = {len(self.tickers)}')
+        for i in range(1,len(self.tickers[0:2])):
+            try:    
+                data = self.parse_data(self.get_data(self.tickers.iloc[i].Symbol))
+            except Exception as e:
+                self.errors_dicts_list.append({
+                    'tick':self.tickers.iloc[i], 'position':i, 'error':str(e)
+                })
+        
+        if self.errors_dicts_list:
+            pd.DataFrame(self.errors_dicts_list).to_csv(
+                file_path,
+                mode='a', header=not os.path.exists(file_path)
+                )
+            print('Some errors occured, check errores file')
+        print('Scarper Finished')
+    
+
+    # def __del__(self):
+    #     pd.DataFrame(self.errors_dicts_list).to_csv('errors.csv')
+
+
+if __name__ == '__main__':
+    cls = cdb()
+    sc = Scarper(cls)
+    cls.show_dbs()
+    sc.main()
